d2ad1408554a6bae9f6cacb9ca271b156ff7b4e3
davidnegelen <david@negelen.de>
2025-10-31 11:29:13 +0100

    test commit

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -2,8 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Welcome to CodeIgniter 4!</title>
-    <meta name="description" content="The small framework with powerful features">
+    <title>team 02</title>
+    <meta name="description" content="test test test">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/png" href="/favicon.ico">
 
